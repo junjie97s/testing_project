f3f9a8e065205f8f73b8f64bdd44819b7cc0fe4a
junjie97s <junjie199739@gmail.com>
2025-05-07 09:15:15 +0800

    Update Testing.js

--- Testing.js
+++ Testing.js
@@ -1,1 +1,6 @@
-System.out.println("Hello World");
+import java.util.Scanner;
+
+Scanner s = new Scanner(System.in)
+System.out.println("Hello World, please insert your name");
+String name = s.nextLine();
+System.out.println("Good Morning " + name);
